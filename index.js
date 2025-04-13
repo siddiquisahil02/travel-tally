763d12f3024a1cac46459330be98121606b105a8
Sahil Siddiqui <siddiquisahil02@gmail.com>
2025-04-13 17:47:58 +0530

    added cors

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-// const cors = require('cors')
+const cors = require('cors')
 const dotenv = require('dotenv')
 
 dotenv.config()
 
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
